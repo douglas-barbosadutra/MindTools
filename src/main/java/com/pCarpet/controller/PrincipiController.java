32fde3b59fb7562dc67cc719260e8b325ce0ce27
AngeloCalaciura <angelodomenicocalaciura@gmail.com>
2019-03-19 16:41:43 +0100

    ShowAll e Seleziona dei Principi

---
+++ src/main/java/com/pCarpet/controller/PrincipiController.java
@@ -0,0 +1,47 @@
+package com.pCarpet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.pCarpet.dto.PrincipiDTO;
+
+import com.pCarpet.services.PrincipiService;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
+@RequestMapping("/homeUser")
+public class PrincipiController {
+	
+	private PrincipiService principiService;
+	
+	@Autowired
+	public PrincipiController(PrincipiService principiService) {
+		this.principiService = principiService;
+		
+	}
+	
+	@RequestMapping(value = "/ShowAll", method = RequestMethod.GET)
+	public String ShowAll(HttpServletRequest request, Model model) {
+		 List <PrincipiDTO> listaPrincipi = principiService.getAllPrincipi();
+		 model.addAttribute("listaPrincipi", listaPrincipi);
+		 return "listaPrincipi";	
+	}
+	
+	@RequestMapping(value = "/SelezionaPrincipio", method = RequestMethod.GET)
+	public String SelezionaPrincipio(HttpServletRequest request, Model model) {
+		int id = Integer.parseInt(request.getParameter("id_principi"));
+		PrincipiDTO principio = principiService.getPrincipio(id);
+		model.addAttribute("Principio", principio);
+		return "SelezionaPrincipio";
+	}
+	
+	
+	
+}
---
+++ src/main/webapp/SelezionaPrincipio.jsp
@@ -0,0 +1,45 @@
+    <%@ page import="com.pCarpet.dto.PrincipiDTO" %>
+    <%@ page import="com.pCarpet.dto.UserDTO" %>
+    <%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+    <!DOCTYPE html>
+<html>
+	<head>
+	
+		<meta charset="UTF-8">
+		<link href = "Stile.css" rel = "stylesheet" media = "screen">
+		<%PrincipiDTO principio = (PrincipiDTO) request.getAttribute("principio"); %>
+		<% int  idprincipio =principio.getIdPrincipi();%>
+		
+		<title>SelezionaPrincipio</title>
+	</head>
+	
+	
+	<body>
+	<form class="form-signin" action="/homeUser/SelezionaPrincipio" method="GET">
+	<table>
+	
+	<img src="/card/<%=idprincipio%>front.jpg">     
+
+	<img src="/card/<%=idprincipio%>retro.jpg">     
+	
+	
+	
+             <a href="homeExperienze/InserisciExperience&principio=<%=principio%>">Insert Experience</a>
+             
+              
+         </td>
+				
+				
+		</tr>
+	</table>
+	</form>
+	
+	 <form action="PrincipiServlet" method=post>
+		<button type="submit" value="Indietro" name="richiesta">Indietro</button>
+	</form>
+	
+	
+	</body>
+	</html>
