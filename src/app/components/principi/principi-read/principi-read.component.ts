64a3363436b345afbaec03d1b73199a937ac751c
unknown <maurizio.dp9@gmail.com>
2019-03-28 17:19:26 +0100

    principi service e component

--- src/app/components/principi/principi-read/principi-read.component.ts
+++ src/app/components/principi/principi-read/principi-read.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Principi } from 'src/app/models/Principi';
+import { PrincipiService } from 'src/app/services/principi/principi.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-principi-read',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipiReadComponent implements OnInit {
 
-  constructor() { }
+  public principi : Array<Principi>;
 
-  ngOnInit() {
-  }
+     constructor(private principiService:PrincipiService, private router: Router){
 
+     }
+
+     ngOnInit(){
+         this.principiService.readPrincipi().subscribe((response) => {
+             this.principi = response;
+         }) 
+        }
+
+        
 }
---
+++ src/app/services/principi/principi.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Principi } from 'src/app/models/Principi';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrincipiService {
+
+  constructor(private http:HttpClient) { }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+        console.error(error);
+        console.log('${operation} failed: ${error.message}');
+        return of(result as T);
+    };
+}
+
+readPrincipi():Observable <Array<Principi>> {
+  return this.http.get<Array<Principi>> ("http://localhost:8080/homeUser/ShowAllPrincipi")
+}
+
+}
