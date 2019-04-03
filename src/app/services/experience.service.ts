b3a6177dbe7c2124bd960c1b1c4eecbaf93129b6
francescoammendolia <pitagora440@gmail.com>
2019-04-03 11:38:32 +0200

    experience by francesco il matematico chitarrista

--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -14,8 +14,8 @@ export class ExperienceService {
   insertExperience(experience : Experience){
     return this.http.post('http//localhost:8080/Experience/insertExperience', experience);
   }
-  showAllExperiences():Observable<any>{
-    return this.http.get<any> ("http://localhost:8080/Experience/showAllExperience");
+  showAllExperiences():Observable<Array<ExperienceUserFeedbackDTO>>{
+    return this.http.get<Array<ExperienceUserFeedbackDTO>> ("http://localhost:8080/Experience/showAllExperience");
   }
 
 }
