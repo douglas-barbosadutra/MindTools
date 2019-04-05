d78a3d5e2f5b04a27e630c2c843b13d87006ed52
unknown <janevito2014@gmail.com>
2019-04-05 19:48:48 +0200

    insert imagen ok

--- src/app/components/experience/insert-experience/insert-experience.component.ts
+++ src/app/components/experience/insert-experience/insert-experience.component.ts
@@ -1,35 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import{Experience} from 'src/app/models/Experience';
 import{Principi} from 'src/app/models/Principi';
 import {ExperienceService} from 'src/app/services/experience.service';
 import{PrincipiService} from 'src/app/services/principi/principi.service';
+import { Imagen } from 'src/app/models/Imagen';
+import { HttpClient} from '@angular/common/http';
+import {ImagenService} from 'src/app/services/imagen.service';
+import {HttpHeaders } from '@angular/common/http';
 
+
+import { User } from 'src/app/models/User';
 @Component({
   selector: 'app-insert-experience',
   templateUrl: './insert-experience.component.html',
   styleUrls: ['./insert-experience.component.css']
 })
+@Injectable({
+  providedIn: 'root'
+})
 export class InsertExperienceComponent implements OnInit {
-
+public selectedFile:File = null;
+  
   private experience : Experience;
   public secondari: Array<Principi>;
+  private imagen:Imagen;
+  private user:Number;
+  public bytes: Array<ArrayBufferTypes>;
+  
+  onFileSelected(event){
+    this.selectedFile = <File>event.target.files[0];
+    
+    
+   
+  }
 
-  constructor(private router: Router, private experienceService: ExperienceService, private principiService: PrincipiService) { }
+  constructor(private router: Router, private experienceService: ExperienceService, private principiService: PrincipiService, private http: HttpClient,
+    private ImagenService: ImagenService) { }
 
   ngOnInit() {
 
-    this.principiService.readPrincipi().subscribe((response) => {
+    this.principiService.readPrincipi().subscribe((response)=> {
       this.secondari = response;
      console.log('Lista principi input caricarita');
- });
+
+  //   this.user = parseInt(sessionStorage.getItem("user");
+  
+ }); 
+ 
   }
-   insertExperience(f: NgForm, imagen: NgForm){
-     this.experience = new Experience(null, null,f.value.commento,f.value.positivo,f.value.negativo,f.value.score, f.value.secondari, imagen.value.imagen);
+   insertExperience(f: NgForm){
+     this.experience = new Experience(null, null,f.value.commento,f.value.positivo,f.value.negativo,f.value.score, f.value.secondari, null);
      this.router.navigateByUrl("allExperience");
-   
    }
+
+   onUpload(){
+     this.ImagenService.pushFileToStorage( this.selectedFile).subscribe(res=>{
+      console.log(res);
+      this.router.navigateByUrl('/InsertExperience');
+    });
+  }
+
    
 
 }
--- src/app/services/imagen.service.ts
+++ src/app/services/imagen.service.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Imagen } from 'src/app/models/Imagen';
 import { tap, catchError } from 'rxjs/operators';
+import { Form, NgForm } from '@angular/forms';
 @Injectable({
   providedIn: 'root'
 })
 export class ImagenService {
+  public selectedFile: File = null;
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-        console.error(error);
-        console.log('${operation} failed: ${error.message}');
-        return of(result as T);
+      console.error(error);
+      console.log('${operation} failed: ${error.message}');
+      return of(result as T);
     };
-}
-insertImagen(imagen: Imagen){
-  return this.http.post('http://localhost:8080/Imagen/savefile', imagen);
-}
+  }
+
+  pushFileToStorage(file: File) {
+    let formdata: FormData = new FormData();
+    formdata.append('file', file);
+   // alert(formdata);
+    return this.http.post('http://localhost:8080/Imagen/savefile', formdata);
+  }
+
+  insertImagen(file: NgForm) {
+    return this.http.post('http://localhost:8080/Imagen/savefile?this.selectedFile=', file);
+  }
+  upload(file: FormData) {
+    return this.http.post('http://localhost:8080/Imagen/upload', file);
+  }
+
+
 
 
 }
