f26e2bf50bbc53299e30af9010d7f6df7a8942b1
GiusyRazzano <giuseppina-razzano@virgilio.it>
2019-04-10 15:47:08 +0200

    Grafica menu

--- src/app/models/FeedbackPrincipiExperience.ts
+++ src/app/models/FeedbackPrincipiExperience.ts
@@ -1,26 +1,25 @@
 import  {Experience} from './Experience';
 import  {Principi} from './Principi';
-import  {Imagen} from './Imagen';
+
 import  {Feedback} from './Feedback';
 
 export class FeedbackPrincipiExperience{
     
     id_experience: number;
     id_principi: number;
-    nome_principioITA: String;
-    nome_principioING: String; 
+    
     secondario: Principi[];
-    imagen: Imagen;
+  
 
     // tslint:disable-next-line:max-line-length
-    constructor(  id_experience: number, id_principi: number, nome_principioITA: String,nome_principioING: String, secondario: Principi[], imagen: Imagen)
+    constructor(  id_experience: number, id_principi: number, secondario: Principi[])
     {
         this.id_experience = id_experience;
         this.id_principi= id_principi;
-        this.nome_principioITA = nome_principioITA;
-        this.nome_principioING = nome_principioING;
+        
+        
         this.secondario= secondario;
-        this.imagen= imagen;
+       
         
        
         
