596d56ef2b61dc67801dfad27af8833a294ba7ad
Giacomo3699 <giacomoerra3699@gmail.com>
2019-04-12 09:32:43 +0200

    parametri-ts

--- src/app/components/parametri/parametri.component.ts
+++ src/app/components/parametri/parametri.component.ts
@@ -3,7 +3,10 @@ import { Router } from "@angular/router";
 import {Parametri} from 'src/app/models/Parametri';
 import {ParametriService} from 'src/app/services/parametri.service';
 import { NgForm } from '@angular/forms';
-
+import { AngularDualListBoxModule } from 'angular-dual-listbox'; 
+import { Key } from 'protractor';
+import {Queue} from 'queue-typescript';
+import { of } from 'rxjs';
 
 
 @Component({
@@ -16,7 +19,8 @@ export class ParametriComponent implements OnInit {
   private parametriList : Array<Parametri>;
   private parametriSceltiList : Array<Parametri>;
   private parametri: Parametri;
-  public nome: string;
+  private parametriSelezionati : Queue<Parametri>;
+  
   
   constructor(private parametriService: ParametriService, private router:  Router) { }
   
@@ -31,11 +35,40 @@ export class ParametriComponent implements OnInit {
        
       }
     });
+
+    let numbers: Parametri[] = [];
+    this.parametriSelezionati = new Queue<Parametri>(... numbers);
+  }
+
+   public choose(id: String) : void 
+  {
+    
+    console.log(id);
+    if(this.parametriSelezionati.length == 2){
+      this.parametriSelezionati.removeHead();
+    }
+    
+    for(let parametri of this.parametriList)
+    {
+      
+      if(parametri.id == 1)
+      this.parametriSelezionati.append(parametri);
+    }
+    
+    console.log(this.parametriSelezionati);
+
+    
+    
+
   }
 
 
-    
-    
-    
+
+  
+
+
   }
+    
+    
+  
   
